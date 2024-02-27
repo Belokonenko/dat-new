83103954956dc13304195869b10a120250d66936
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2024-02-27 19:30:34 +0200

    Fix : nav-prod

--- src/js/modules/nav-prod.js
+++ src/js/modules/nav-prod.js
@@ -1,12 +1,15 @@
 export default function navProd() {
-    const navProd = document.querySelector('.nav-prod');
+    const navProd = document.querySelector(".nav-prod");
 
     if (navProd) {
-        const btnMenu = navProd.querySelector('.nav-prod__btn-menu');
-
-        btnMenu.addEventListener('click', () => {
-            navProd.classList.toggle('nav-prod_active');
+        const btnMenu = navProd.querySelector(".nav-prod__btn-menu");
+        const btn = navProd.querySelectorAll(".nav-prod__btn");
+        btnMenu.addEventListener("click", () => {
+            navProd.classList.toggle("nav-prod_active");
         });
 
+        btn.addEventListener("click", () => {
+            navProd.classList.remove("nav-prod_active");
+        });
     }
 }
