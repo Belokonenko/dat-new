e4ac93f3cf1fef4f0100d7db31d3bc47ce5e8420
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2024-03-11 22:06:21 +0200

    fix : text

--- src/js/modules/seeds.js
+++ src/js/modules/seeds.js
@@ -6,7 +6,7 @@ export default function seeds() {
         const textHidden = blockSeeds.querySelector(".seeds__text-hidden");
 
         btn.addEventListener("click", () => {
-            console.log("clic hidden text");
+            console.log("clic hidden text_faded ");
             textHidden.classList.add("seeds__text_active");
             btn.style.display = "none";
         });
