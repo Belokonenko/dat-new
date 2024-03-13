6b29d31c000344fcc426314205fe3d7d73f2b576
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2024-03-13 08:04:08 +0200

    save

--- src/js/modules/validateAllFilds.js
+++ src/js/modules/validateAllFilds.js
@@ -9,45 +9,9 @@ export default function validateAllFilds() {
     const agreeCheckbox = document.getElementById("agree");
     const signUpButton = document.querySelector(".registration__button");
 
-    // // Функция для проверки корректности имени
-    // function validateName() {
-    //     // Ваш код валидации для поля имени
-    // }
-
-    // Функция для проверки корректности фамилии
-    function validateSurname() {
-        // Ваш код валидации для поля фамилии
-    }
-
-    // Функция для проверки корректности номера телефона
-    function validatePhone() {
-        // Ваш код валидации для поля номера телефона
-    }
-
-    // Функция для проверки корректности пароля
-    function validatePassword() {
-        // Ваш код валидации для поля пароля
-    }
-
-    // Функция для проверки совпадения пароля и подтверждения пароля
-    function validateConfirmPassword() {
-        // Ваш код валидации для поля подтверждения пароля
-    }
-
-    // Функция для проверки согласия на обработку персональных данных
-    function validateAgreement() {
-        // Ваш код валидации для флажка согласия
-    }
 
     // Обработчик события клика по кнопке "Sign up"
     signUpButton.addEventListener("click", function(event) {
-        // Вызываем все функции валидации
-        // validateName();
-        // validateSurname();
-        // validatePhone();
-        // validatePassword();
-        // validateConfirmPassword();
-        // validateAgreement();
 
         // Предотвращаем отправку формы, если есть ошибки валидации
         if (document.querySelector(".error")) {
