706987c26673200ee77f5d86c55e816e5e458499
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2024-03-20 12:20:33 +0200

    add custom-selector

--- src/js/modules/custom-select.js
+++ src/js/modules/custom-select.js
@@ -1,18 +1,26 @@
 export default function customSelect() {
-    var select = document.getElementById("mySelect");
-    var selectStyled = document.querySelector(".select-styled");
-    var selectOptions = document.querySelector(".select-options");
+    const customSelects = document.querySelectorAll(".custom-select");
 
-    selectStyled.addEventListener("click", function () {
-        selectOptions.style.display =
-            selectOptions.style.display === "none" ? "block" : "none";
-    });
+    if (customSelects) {
+        customSelects.forEach((item) => {
+            console.log(item);
 
-    selectOptions.addEventListener("click", function (event) {
-        if (event.target.tagName === "LI") {
-            select.value = event.target.getAttribute("data-value");
-            selectStyled.textContent = event.target.textContent;
-            selectOptions.style.display = "none";
-        }
-    });
+            const select = item.querySelector(".mySelect");
+            const selectStyled = item.querySelector(".select-styled");
+            const selectOptions = item.querySelector(".select-options");
+
+            selectStyled.addEventListener("click", function () {
+                selectOptions.style.display =
+                    selectOptions.style.display === "none" ? "block" : "none";
+            });
+
+            selectOptions.addEventListener("click", function (event) {
+                if (event.target.tagName === "LI") {
+                    select.value = event.target.getAttribute("data-value");
+                    selectStyled.textContent = event.target.textContent;
+                    selectOptions.style.display = "none";
+                }
+            });
+        });
+    }
 }
